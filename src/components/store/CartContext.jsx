1b987ddb85eaa22dfb6bb0bbefd1d26b488d65d7
SANDEEP SAINI <sandeepsn72@gmail.com>
2024-04-21 23:13:08 +0530

    sorta kinda going on

--- src/components/store/CartContext.jsx
+++ src/components/store/CartContext.jsx
@@ -7,7 +7,14 @@ const CartContext = createContext({
 });
 function cartReducer(state, action) {
   if (action.type === "ADD_ITEM") {
-    state.items.push(action.item);
+    const existingCartItemIndex = state.items.findIndex(
+      (item) => item.id === action.item.id
+    );
+  }
+  const updatedItems = [...state.items];
+  if (existingCartItemIndex > -1) {
+  } else {
+    updatedItems.push(action.item);
   }
 
   if (action.type === "REMOVE_ITEM") {
