7b50e0331e7fc74190e61824fdde0c7fd528f6dc
SANDEEP SAINI <sandeepsn72@gmail.com>
2024-05-05 23:56:21 +0530

    more and more

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import Header from "./components/Header";
 import Meals from "./components/Meals";
 import { CartContextProvider } from "./components/store/CartContext";
+import { UserProgressContextProvider } from "./components/store/UserProgressContext.jsx";
 function App() {
   return (
-    <CartContextProvider>
-      <Header />;
-      <Meals />
-    </CartContextProvider>
+    <UserProgressContextProvider>
+      <CartContextProvider>
+        <Header />;
+        <Meals />
+      </CartContextProvider>
+    </UserProgressContextProvider>
   );
 }
 
