e9952188e2c43a76de65fc222a238a58fa9e60f5
SANDEEP SAINI <sandeepsn72@gmail.com>
2024-05-06 23:59:34 +0530

    usercontext

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,8 +2,10 @@ import { useContext } from "react";
 import logoImg from "../assets/logo.jpg";
 import Button from "./UI/Button.jsx";
 import CartContext from "./store/CartContext.jsx";
+import UserProgressContext from "./store/UserProgressContext.jsx";
 export default function Header() {
   const cartCtx = useContext(CartContext);
+  useContext(UserProgressContext);
   console.log(cartCtx);
   const totalCartItems = cartCtx.items.reduce((totalNumberOfItems, item) => {
     return totalNumberOfItems + item.quantity;
